14177ec5ec577e7b28a097d22915b910e4ed44fb
taylorsantosoliveira@hotmail.com <Suco071952804$>
2023-12-05 22:04:10 -0300

    chore: Core Refactoring

--- src/main/java/org/code/TwoSum.java
+++ src/main/java/org/code/TwoSum.java
@@ -1,17 +1,19 @@
 package org.code;
 
+import java.util.Arrays;
+
 public class TwoSum {
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
 
         int target1 = 9;
-        System.out.println(twoSum(nums, target1));
+        System.out.println(Arrays.toString(twoSum(nums, target1)));
 
         int target2 = 6;
-        System.out.println(twoSum(nums, target2));
+        System.out.println(Arrays.toString(twoSum(nums, target2)));
 
         int target3 = 18;
-        System.out.println(twoSum(nums, target3));
+        System.out.println(Arrays.toString(twoSum(nums, target3)));
     }
 
     public static int[] twoSum(int[] nums, int target) {
